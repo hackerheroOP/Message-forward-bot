cf79d47f15fcdfd7df0e228a587211add7c799ff
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2023-08-10 22:18:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ def forward_messages(bot, chat_id):
     # Implement your message forwarding logic here
     
 if __name__ == "__main__":
+elif print ("error")
     user_data = {}  # Dictionary to store user data
     app.run()
     
